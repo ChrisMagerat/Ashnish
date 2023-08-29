0bb2d8766506e3eb54cf5eec9f0af4b60ee6c54a
Ashish Jakkam <ashishjakkam@gmail.com>
2023-08-29 09:37:28 +0200

    Created Problem Solver Folder

---
+++ Problem_Solver_SubProject/ProblemSolverQuestion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolverQuestion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What string would you like to write out:");
+
+            Console.ReadLine();
+        }
+    }
+}
