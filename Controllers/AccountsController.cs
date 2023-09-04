9391a659cb7ecc64c4ee5ae489875c7b94528cf0
Ashish Jakkam <ashishjakkam@gmail.com>
2023-09-04 16:36:54 +0200

    Final Commit

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ashnish.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetStrings()
+        {
+            string[] stringNames = { "String1", "String2", "String3" };
+            return stringNames;
+        }
+
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace Ashnish.Models;
+
+public class Account
+{
+    public Guid UserId { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+
+}
